9966691c9fca53782e01a08be48a2c6084f5ebe7
Symphony <49285362+symphony701@users.noreply.github.com>
2022-06-24 13:56:48 -0500

    Update MessagesResources.cs

--- backend/Back-end-develop/Messages/Resources/MessagesResources.cs
+++ backend/Back-end-develop/Messages/Resources/MessagesResources.cs
@@ -2,6 +2,7 @@ namespace EasyJob.API.Messages.Resources
 {
     public class MessagesResources
     {
+        //TODO: make relations
         public int Id { get; set; }
         public string Description { get; set; }
         public string Date { get; set; }
