0efeb07a5361b6e14efce90764680c1c3dffa54a
Prado71 <u201711121@upc.edu.pe>
2022-06-26 21:12:32 -0500

    feat: addtestingmessage

---
+++ backend/TestProject1/CreateMessage.feature
@@ -0,0 +1,20 @@
+﻿Feature: CreateMessage
+	as aplicant an postulant
+	i want to send messages.
+	Background: 
+		Given to Endpoint https://localhost:5001/api/v1/Messages
+		And a postualnt is already stored
+		| Id | Name      | LastName | Description       | Email        | Password | GithubUser | Photo     |
+		| 1  | sebastian | roque    | flutter developer | rn@gmail.com | 123123   | symphony   | image.png |
+        And applicant is already stored
+	    | Id | Name | Ruc    | Website | Email | Password | Photo |
+	    | 1  | luis | 032645 |   youtube.com      |   luis@hotmail.com    |   123456       |  image.jpg     |
+	
+	
+@post-message
+Scenario: Add message
+	When a message request is send
+	    | fromApplicant | Postulant_Id    | Applicant_Id | Date       | Text                |
+	    |     sebastian |    1            |       1      |  20-10-21  |  hola como estas    |
+	Then a Response with status 200 is received
+    
---
+++ backend/TestProject1/CreateMessageStepsDefinition.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using EasyJob.API;
+using EasyJob.API.Applicants.Resources;
+using EasyJob.API.Messages.Resources;
+using EasyJob.API.Postulants.Resources;
+using EasyJob.API.Projects.Resources;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SpecFlow.Internal.Json;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace TestProject1;
+
+[Binding]
+public class CreateMessageStepsDefinition
+{
+    private readonly WebApplicationFactory<Startup> _factory;
+
+    public CreateMessageStepsDefinition(WebApplicationFactory<Startup> factory)
+    {
+        _factory = factory;
+    }
+
+    private HttpClient _client { get; set; }
+    private Uri baseUri { get; set; }
+    private Task <HttpResponseMessage> response { get; set; }
+    private PostulantResource postulant { get; set; }
+    private ApplicantResource applicant { get; set; }
+    private MessagesResources message { get; set; }
+    [Given(@"to Endpoint https://localhost:(.*)/api/v(.*)/Messages")]
+    public void GivenToEndpointHttpsLocalhostApivMessages(int port, int version)
+    {
+        baseUri = new Uri($"https://localhost:{port}/api/v{version}/Messages");
+        _client = _factory.CreateClient(new WebApplicationFactoryClientOptions {BaseAddress = baseUri});
+    }
+
+    [Given(@"applicant is already stored")]
+    public void GivenApplicantIsAlreadyStored(Table applicantResource)
+    {
+        var resource = applicantResource.CreateSet<SaveApplicantResource>().First();
+        var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+        response = _client.PostAsync(baseUri, content);
+    }
+
+    [Given(@"postulant is already stored")]
+    public void GivenPostulantIsAlreadyStored(Table postulantResource)
+    {
+        var resource = postulantResource.CreateSet<SavePostulantResource>().First();
+        var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+        response = _client.PostAsync(baseUri, content);
+    }
+    
+    
+    [When(@"a message request is send")]
+    public void WhenAMessageRequestIsSend(Table messageResource)
+    {
+        var resource = messageResource.CreateSet<SaveMessageResource>().First();
+        var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+        response = _client.PostAsync(baseUri, content);
+    }
+    
+    [Then(@"a Response with status (.*) is received")]
+    public void ThenAResponseWithStatusIsReceived(int spectStatus)
+    {
+        var spectStatusCode = ((HttpStatusCode)spectStatus).ToString();
+        var actualStatusCode = response.Result.StatusCode.ToString();
+        Assert.Equal(spectStatusCode,actualStatusCode);
+    }
+}
